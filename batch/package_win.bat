e5e97a4bcd07c524f7be29baac92ca1d58267fa1
zanisuke <zanisuke@gamil.com>
2021-10-11 02:39:46 +0900

    fix: win batch

--- batch/package_win.bat
+++ batch/package_win.bat
@@ -2,6 +2,8 @@ set ROOT_PATH=%~dp0..
 set ENGINE_PATH=%ROOT_PATH%\..\..\engine\ue4.26\Engine
 set RUN_UAT_BAT=%ENGINE_PATH%\Build\BatchFiles\RunUAT.bat
 
+set uebp_LogFolder=D:\work\unreal_plugin\projects\sample\BuildLog
+
 %RUN_UAT_BAT% BuildCookRun ^
     -nocompile ^
     -nocompileeditor ^
